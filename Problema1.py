1580cbb2a147ba582868fad8dbf5ccc3dace5b70
Ender1GG <1carlos1.survivalrolemine@gmail.com>
2025-06-27 16:23:13 +0000

    PC LISTA

---
+++ Problema1.py
@@ -0,0 +1,31 @@
+# nombre=input("Ingrese su nombre de usuario: ")
+#print(f"! Hola {nombre} !")
+
+#consumo=float(input("Consumo : "))
+#porc_propina=int(input("Porcentaje de propina a dejar: "))
+#propina=consumo*porc_propina/100
+#print(f"Cantidad de dinero a dejar como propina: {propina} ")
+
+#payaso_peso=112
+#muneca_peso=75
+#payasos_vend=int(input("Payasos vendidos ultimo paquete= "))
+#munecas_vend=int(input("Munecas vendidas ultimo paquete: "))
+#peso_paquete=payasos_vend*payaso_peso+munecas_vend*muneca_peso
+#print(f"Peso total del ultimo paquete a enviar: {peso_paquete} gr")
+
+#N=int(input("Ingrese el entero positivo : "))
+#suma=(N*(N+1))/2
+#print(f"Suma de los N enteros positivos: {suma}")
+
+#shows_ultimoa=int(input("Shows vistos : "))
+#print(shows_ultimoa>3)
+
+#edad=int(input("Ingrese su edad: "))
+#if edad < 4 :
+#    print("Ingrese gratis")
+#elif 4 <= edad <= 18 :
+#    print("El costo es $5")
+#elif edad>18 :
+#    print("El costo es $10")
+
+hora=
