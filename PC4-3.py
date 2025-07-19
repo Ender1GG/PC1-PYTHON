0458e77548b0177d356ab89914c07e21f210741d
Ender1GG <1carlos1.survivalrolemine@gmail.com>
2025-07-19 15:29:44 +0000

    pc4

--- PC4-3.py
+++ PC4-3.py
@@ -12,9 +12,9 @@ def contar_loc(ruta):
             if linea_strip and not linea_strip.startswith("#"):
                 loc += 1
 
-        print(f"Número de líneas de código: {loc}")
+        print(f"Numero de lineas de codigo: {loc}")
     except FileNotFoundError:
-        print("Ruta no válida o archivo no existe.")
+        print("Ruta no valida o archivo no existe.")
 
 ruta = input("Ingrese la ruta del archivo .py: ")
 contar_loc(ruta)
